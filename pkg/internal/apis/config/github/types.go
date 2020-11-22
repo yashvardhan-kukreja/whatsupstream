681ad41c11e4eb0feaf0c918501eb2fc2268e80e
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-11-23 02:00:37 +0530

    made some tweaks

--- pkg/internal/apis/config/github/types.go
+++ pkg/internal/apis/config/github/types.go
@@ -1,13 +1,29 @@
+/*
+Copyright © 2020 Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package github
 
 // Issue represents the fields coming from the API response from api.github.com
 type Issue struct {
 	Title string `json:"title"`
-	IssueNumber int32 `json:"number"`
+	Number int32 `json:"number"`
 	State string `json:"state"`
 	CreatedAt string `json:"created_at"`
 	User User `json:"user"`
 	Labels []Label `json:"labels"`
+	RepositoryURL string `json:"repository_url"`
 }
 
 type User struct {
