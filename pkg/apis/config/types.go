54dd46027a65b757cc03db360bb5d9b6f7e911fe
Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
2020-11-23 00:12:15 +0530

    setup: functionalities associated with incoming config from the host

---
+++ pkg/apis/config/types.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2020 Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package config
+
+// Config represents the main configuration on the basis of which the user will get notifications
+type Config struct {
+
+	// IssueConfigs is the array of multiple configurations associated with multiple kinds of issue-related notifications.
+	// for example - an element of IssueConfigs can be responsible for notifying about "good first issue" labelled issues,
+	// and another element of IssueConfigs can be responsible for notifying about issues associated with "bug" labelled issues of another repo.
+	IssueConfigs []IssueConfig `yaml:"issue-configs,omitempty"`
+}
+
+// IssueConfig represents the configuration associated with issue-related notifications of a repository
+type IssueConfig struct {
+
+	// RepositoryURL denotes the URL of the repository for which you want the receive the issue-related notifications.
+	RepositoryURL string `yaml:"repository-url,omitempty"`
+
+	// Labels denotes the list of labels which the issues (of which you want to get notified) must contain.
+	Labels []string `yaml:"labels,omitempty"`
+
+	// Assignees denotes the person who must be assigned to the issue, for that issue being eligible for being notified about.
+	// if left unset, then, it will be set to "*" denoting the issues can be assigned to any user.
+	Assignee string `yaml:"assignee,omitempty"`
+
+	// Creator denotes the creator from whom the issue must be created, so as to be eligible for being notified about.
+	Creator string `yaml:"creator,omitempty"`
+
+	// Closed denotes whether to include closed issues too (along the open ones).
+	Closed bool `yaml:"closed,omitempty"`
+
+	// Since denotes the timestamp from which the issues which were created after it, will only be eligible for being notified about.
+	Since string `yaml:"since,omitempty"`
+}
+
+const (
+	API_BASE_URL_REPOS = "https://api.github.com/repos"
+)
---
+++ pkg/apis/config/validators.go
@@ -0,0 +1,61 @@
+/*
+Copyright © 2020 Yashvardhan Kukreja <yash.kukreja.98@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package config
+
+import (
+	"fmt"
+	"regexp"
+	"net/http"
+)
+
+const (
+	GITHUB_URL_REGEX = "(?:git|ssh|https?|git@[-w.]+)(:)?(//)?(.*?)(.git)?(/?|#[-dw._]+?)$"
+)
+
+func ValidateConfig(config Config) error {
+	for _, issueConfig := range config.IssueConfigs {
+		if err := validateIssueConfig(issueConfig); err != nil {
+			return fmt.Errorf("error occurred while parsing the provided input configuration: %w", err)
+		}
+	}
+	return nil
+}
+
+func validateIssueConfig(issueConfig IssueConfig) error {
+	if err := validateRepositoryURL(issueConfig.RepositoryURL); err != nil {
+		return fmt.Errorf("error occurred while parsing an issue configuration: %w", err)
+	}
+	if err := validateSince(issueConfig.Since); err != nil {
+		return fmt.Errorf("error occurred while parsing an issue configuration: %w", err)
+	}	
+	return nil
+}
+
+func validateSince(since string) error {
+	return nil
+}
+
+func validateRepositoryURL(repositoryURL string) error {
+	if matches, _ := regexp.MatchString(GITHUB_URL_REGEX, repositoryURL); !matches {
+		return fmt.Errorf("repository URL not provided rightfully")
+	}
+	resp, err := http.Get(repositoryURL)
+	if err != nil || resp.StatusCode >= 400 {
+		return fmt.Errorf("repository URL not provided rightfully")
+	}
+	defer resp.Body.Close()
+	return nil
+}
